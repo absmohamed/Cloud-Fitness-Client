e87499c97535e5924ec61e4eca624b8ce2fa6e9f
raeginez <rachelginez@yahoo.com>
2020-01-13 19:42:23 +1000

    added pricing image

--- src/components/pricing/pricing.js
+++ src/components/pricing/pricing.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./pricing.css";
+import Price from '../media/price.jpg'
 
 function Pricing() {
     return (
         <div>
             <h1> The Pricing page </h1>
+            <img src={Price} alt="pricing" />
         </div>
     );
 };

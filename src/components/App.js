b3033e852a51dee3c43a1dab3504d748b96f0332
raeginez <rachelginez@yahoo.com>
2020-01-13 15:09:05 +1000

    top nav added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import About from "./about"
+import Toolbar from "./Toolbar/Toolbar";
 
 function App() {
   return (
-    <div >
-      <Switch>
-        <Route path="/about" component={About} /> 
-      </Switch>
+    <div className="App">
+      <Toolbar/>
+      <div >
+        <Switch>
+          <Route path="/about" component={About} /> 
+        </Switch>
+      </div>
     </div>
+
   );
 }
 

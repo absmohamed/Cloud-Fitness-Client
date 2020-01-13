40378aba27f2b054f6e10acba68a19b698ecf602
raeginez <rachelginez@yahoo.com>
2020-01-13 17:42:37 +1000

    added routing

---
+++ src/components/services/services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./services.css";
+
+function Services() {
+    return (
+        <div>
+            <h1> The Services page </h1>
+        </div>
+    );
+};
+
+export default Services;

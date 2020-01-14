7d4227623fb0be05561cbd7f8d3d71a8b87c8911
raeginez <rachelginez@yahoo.com>
2020-01-14 14:55:32 +1000

    added pictures on services page

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./services.css";
+import { Link } from 'react-router-dom';
+import Flexandrelax from '../media/flexandrelax.jpg'
+import Strength from '../media/strength.jpg'
+import Aerobic from '../media/aerobic.jpg'
 
 function Services() {
     return (
         <div>
             <h1> The Services page </h1>
+            <img src={Flexandrelax} alt="pricing" />
+            <img src={Strength} alt="strength" />
+            <img src={Aerobic} alt="aerobic" />
         </div>
     );
 };

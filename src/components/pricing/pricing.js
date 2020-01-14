b6b49a2629a124dd5b87fa0083d5911e710455c0
vicnatacu <vicki@mvbmb.com>
2020-01-14 16:20:14 +1000

    adding pricing button

--- src/components/pricing/pricing.js
+++ src/components/pricing/pricing.js
@@ -2,11 +2,20 @@ import React from 'react';
 import "./pricing.css";
 import Price from '../media/price.jpg'
 
-function Pricing() {
+
+
+function Pricing({history}) {
+    function showServices () {
+        history.push("/services")
+    }
     return (
         <div>
             <h1> The Pricing page </h1>
             <img src={Price} alt="pricing" />
+            <form>
+                <imput type="submit" name="Services" className="button is-info" onClick={showServices}></imput>
+            </form>
+           
         </div>
     );
 };

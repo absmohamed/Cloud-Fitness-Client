76a7c36f97f74aa0cf202d06d12a67bd128ee533
raeginez <rachelginez@yahoo.com>
2020-01-17 12:32:23 +1000

    added styling

---
+++ src/components/backdrop/backdrop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './backdrop.scss'
+
+
+const Backdrop = props => (
+    <div className="backdrop"/>
+
+);
+
+export default Backdrop;
---
+++ src/components/drawerToggleButton/drawerToggleButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import 'drawerToggleButtton.scss'
+
+const DrawerToggleButtton = props => (
+    <button className="toggle-button">
+        <div className="toggle-button_line"/>
+        <div className="toggle-button_line"/>
+        <div className="toggle-button_line"/>
+    </button>
+);
+
+
+
+export default DrawerToggleButtton;
